d472e3fe9cd95f142c605278f7bb00cb6094da99
Dan Nuffer <danielnuffer@gmail.com>
2014-03-09 16:51:56 -0600

    Adjust hi_res.sh settings.

--- hi_res.sh
+++ hi_res.sh
@@ -3,12 +3,12 @@ set -x
 set -e
 set -u
 
-if [ $UID != "0" ]; then
-	echo "This script must be run as root" >&2
+if [ $UID = "0" ]; then
+	echo "This script must not be run as root" >&2
 	exit 1
 fi
 
-patch -p0 -N -r- /usr/share/themes/Ambiance/metacity-1/metacity-theme-1.xml < metacity-theme-1.xml.patch
-gsettings set org.gnome.desktop.interface scaling-factor 2
-gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
+sudo patch -p0 -N -r- /usr/share/themes/Ambiance/metacity-1/metacity-theme-1.xml < metacity-theme-1.xml.patch
+#gsettings set org.gnome.desktop.interface scaling-factor 2
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.3
 echo "Open System Settings->Displays and change UI Scale to 1.5"
