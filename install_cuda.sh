b3674d61f0a1c193a1b3741ec78a73d08d97b25e
Dan Nuffer <danielnuffer@gmail.com>
2014-12-23 14:50:11 -0700

    Add install_cuda.sh

---
+++ install_cuda.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+set -e
+set -u
+
+if [ $UID != "0" ]; then
+	echo "This script must be run as root" >&2
+	exit 1
+fi
+
+# The nvidia driver sucks for desktop use :-(
+if ! [ -e /etc/apt/sources.list.d/cuda.list ]; then
+	wget -O /tmp/cuda.deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
+	dpkg -i /tmp/cuda.deb
+	apt-get -y update
+fi
+
+
+apt-get update
+apt-get -y install --fix-broken --ignore-hold --auto-remove \
+	cuda
