6a6112164e37c272fcc966635b5bcbf562536ee2
Dan Nuffer <danielnuffer@gmail.com>
2017-05-06 10:55:03 -0600

    Add insync

--- ubuntu16.04.sh
+++ ubuntu16.04.sh
@@ -15,13 +15,19 @@ if [ ! -e /etc/apt/sources.list.d/recoll-backports-ubuntu-recoll-1_15-on-xenial.
 	apt-get -y update
 fi
 
+if [ ! -e /etc/apt/sources.list.d/insync.list ]; then
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
+	echo 'deb http://apt.insynchq.com/ubuntu xenial non-free contrib' > /etc/apt/sources.list.d/insync.list
+	apt-get -y update
+fi
+
 apt-get update
 
 apt-get -y dist-upgrade
 
 GO_PKGS="golang"
 DOCKER_PKGS="docker.io"
-MY_TOOLS="alarm-clock-applet anki lyx meld gimp git-gui gitk htop httpie ifstat inkscape iotop molly-guard nethogs nmap linux-tools-generic ncdu python-csvkit python3-csvkit whois lm-sensors texlive-full"
+MY_TOOLS="alarm-clock-applet anki lyx meld gimp git-gui gitk htop httpie ifstat inkscape iotop molly-guard nethogs nmap linux-tools-generic ncdu python-csvkit python3-csvkit whois lm-sensors texlive-full insync"
 MISC="tmux"
 
 apt-get install --fix-broken \
