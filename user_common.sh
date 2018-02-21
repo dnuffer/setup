a5194b4cc2060ab6b391d43fbdb5db15e83a4d8f
Dan Nuffer <dan.nuffer@lovelandinnovations.com>
2018-02-21 11:00:03 -0700

    Add ssh-agent startup to .bash_profile.

--- user_common.sh
+++ user_common.sh
@@ -106,3 +106,30 @@ fi
 if ! grep -q termcapinfo ~/.screenrc; then
 	echo "termcapinfo xterm* ti@:te@" >> ~/.screenrc
 fi
+
+if ! grep -q "Initializing new SSH agent" ~/.bash_profile; then
+cat >> ~/.bash_profile << EOS
+SSH_ENV="\$HOME/.ssh/environment"
+
+function start_agent {
+    echo "Initializing new SSH agent..."
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "\${SSH_ENV}"
+    echo succeeded
+    chmod 600 "\${SSH_ENV}"
+    . "\${SSH_ENV}" > /dev/null
+    /usr/bin/ssh-add;
+}
+
+# Source SSH settings, if applicable
+
+if [ -f "\${SSH_ENV}" ]; then
+    . "\${SSH_ENV}" > /dev/null
+    #ps \${SSH_AGENT_PID} doesn't work under cywgin
+    ps -ef | grep \${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+        start_agent;
+    }
+else
+    start_agent;
+fi
+EOS
+fi
